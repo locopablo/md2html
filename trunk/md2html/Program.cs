226f60b1060f724969152915e86cfba09c241a93
lassevk <lassevk@42038788-09f2-11df-b996-818f58f34e26>
2010-01-26 12:31:23 +0000

    Added unit test that verifies console output.

--- trunk/md2html/Program.cs
+++ trunk/md2html/Program.cs
@@ -1,13 +1,12 @@
 ﻿using MarkdownSharp;
 using System;
+
 namespace md2html
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Markdown md = new Markdown();
-            Console.Out.WriteLine(md.Version);
         }
     }
 }
