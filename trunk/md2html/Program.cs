af6368dd9c0a646108b4bcbea21c2985ee31f8d1
lassevk <lassevk@42038788-09f2-11df-b996-818f58f34e26>
2010-01-26 11:50:12 +0000

    Added dummy call to Markdown class in order to test code coverage

--- trunk/md2html/Program.cs
+++ trunk/md2html/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace md2html
+﻿using MarkdownSharp;
+using System;
+namespace md2html
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            Markdown md = new Markdown();
+            Console.Out.WriteLine(md.Version);
         }
     }
 }
