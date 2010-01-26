6816142762e200b560ceb0315fc2c0ed7802df97
lassevk <lassevk@42038788-09f2-11df-b996-818f58f34e26>
2010-01-26 12:31:23 +0000

    Added unit test that verifies console output.

--- md2html.Tests/ConsoleOutputTests.cs
+++ md2html.Tests/ConsoleOutputTests.cs
@@ -3,23 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
+using System.IO;
 
 namespace md2html.Tests
 {
     [TestFixture]
     public class ConsoleOutputTests
     {
-        private string GetOutput(params string[] args)
+        private struct Result
         {
-            Program.Main(args);
-            return string.Empty;
+            public string Output;
+            public string Error;
+            public int ExitCode;
+        }
+
+        private Result GetOutput(params string[] args)
+        {
+            using (StringWriter output = new StringWriter())
+            using (StringWriter error = new StringWriter())
+            {
+                Console.SetOut(output);
+                Console.SetError(error);
+                Environment.ExitCode = 0;
+
+                Program.Main(args);
+
+                Result result = new Result();
+                result.Output = output.ToString();
+                result.Error = error.ToString();
+                result.ExitCode = Environment.ExitCode;
+                return result;
+            }
         }
 
         [Test]
         public void NoInput_NoOutput()
         {
-            string output = GetOutput();
-            Assert.That(output, Is.EqualTo(string.Empty));
+            Result result = GetOutput();
+            Assert.That(result.Output, Is.EqualTo(string.Empty));
+            Assert.That(result.Error, Is.EqualTo(string.Empty));
+            Assert.That(result.ExitCode, Is.EqualTo(0));
         }
     }
 }
