01d35966e7198e13ea3011d6a2dd85d5def244be
lassevk <lassevk@42038788-09f2-11df-b996-818f58f34e26>
2010-01-26 11:26:34 +0000

    Added basic unit test.

---
+++ trunk/md2html.Tests/ConsoleOutputTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace md2html.Tests
+{
+    [TestFixture]
+    public class ConsoleOutputTests
+    {
+        private string GetOutput(params string[] args)
+        {
+            Program.Main(args);
+            return string.Empty;
+        }
+
+        [Test]
+        public void NoInput_NoOutput()
+        {
+            string output = GetOutput();
+            Assert.That(output, Is.EqualTo(string.Empty));
+        }
+    }
+}
